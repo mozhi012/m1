c19a6cc00f9d07eff321c702b80420b57ba8ce96
mozhi012 <mozhi0012@gmail.com>
2021-10-07 18:21:34 +0800

    添加vue.config.js，配置全局CSS样式

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,7 @@
    <router-view></router-view>
   </div>
 </template>
+
+<style lang='scss'>
+
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-      登录页面
-  </div>
+	<div class="login">
+		登录页面
+		<el-button type="primary">主要按钮</el-button>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
-
-<style lang='scss' scoped>
-    div{
-        background-color:red;
-    }
+<style lang="scss" scoped>
+.login {
+	height: 100vw;
+	// background-color: #344a5f;
+}
 </style>
